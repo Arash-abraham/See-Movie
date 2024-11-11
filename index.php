9a345b0f6b7c026db29720f372b149856bba12ac
Arash <82562395+Arash-abraham@users.noreply.github.com>
2024-11-11 21:00:03 +0330

    Update index.php

--- index.php
+++ index.php
@@ -3,11 +3,9 @@ $request_uri = $_SERVER['REQUEST_URI'];
 
 if ($request_uri == '/Login') {
     include 'html/login.html';
-} 
-else if ($request_uri != '/Login') {
-    include 'html/404.html';
-}
-else {
+} else if ($request_uri == '/') {
     include 'html/index.html';
+} else {
+    include 'html/404.html'; 
 }
 ?>
