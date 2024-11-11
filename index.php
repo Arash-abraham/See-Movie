928f5eae7fecaa801ebe28f92bbfc3f873a66f50
Arash <82562395+Arash-abraham@users.noreply.github.com>
2024-11-11 20:19:24 +0330

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,6 @@ $request_uri = $_SERVER['REQUEST_URI'];
 if ($request_uri == '/Login') {
     include 'html/login.html';
 } else {
-    echo "صفحه مورد نظر یافت نشد.";
+    include '404/404.html';
 }
 ?>
