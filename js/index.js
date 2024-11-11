9658d8dadcffc7ab7f33cd7a4d081dadc1d3bd0d
Arash <82562395+Arash-abraham@users.noreply.github.com>
2024-11-11 20:45:16 +0330

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
-alert('Fuck');
+const backgrounds = [
+    '../img/background1.jpg', 
+    '../img/background2.jpg', 
+    '../img/background3.jpg' 
+];
+
+const randomIndex = Math.floor(Math.random() * backgrounds.length);
+document.body.style.backgroundImage = `url('${backgrounds[randomIndex]}')`;
