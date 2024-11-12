c19c870f1f7b69431019232de5464dfc497c45ca
Arash <82562395+Arash-abraham@users.noreply.github.com>
2024-11-12 19:56:14 +0330

    Update 404.js

--- js/404.js
+++ js/404.js
@@ -1,27 +1,3 @@
-let canvas = document.getElementById("canvas");
-let ctx = canvas.getContext("2d");
-
-function resize() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-}
-
-function draw() {
-    ctx.fillStyle = "black";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    let data = imageData.data;
-    for (let i = 0; i < data.length; i += 4) {
-        let color = Math.random() * 255;
-        data[i] = color; // red
-        data[i + 1] = color; // green
-        data[i + 2] = color; // blue
-        data[i + 3] = 255; // alpha
-    }
-    ctx.putImageData(imageData, 0, 0);
-    requestAnimationFrame(draw);
-}
-
-resize();
-draw();
-window.addEventListener("resize", resize);
+document.getElementById('backButton').addEventListener('click', function() {
+    window.location.href = '/'; // Redirect to home page
+});
