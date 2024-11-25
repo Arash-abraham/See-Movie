530f2da6baf98ec929afd0a63a81e25bbd13b052
Arash <82562395+Arash-abraham@users.noreply.github.com>
2024-11-25 21:51:01 +0330

    Create lib.php

---
+++ php/lib.php
@@ -0,0 +1,32 @@
+<?php
+    function connection() {
+        global $dbLink;
+        try {
+            $dbLink = mysqli_connect('localhost', 'root', '');
+            if (!$dbLink) {
+                throw new Exception('Connection failed: ' . mysqli_connect_error());
+            }
+            
+            if (!mysqli_select_db($dbLink, 'moive_db')) {
+                throw new Exception('Database selection failed: ' . mysqli_error($dbLink));
+            }
+
+            mysqli_set_charset($dbLink, 'utf8');
+        }
+        catch (Exception $th) {
+            print $th->getMessage();
+            die();
+        }
+    }
+    function sanitize($data) {
+        return htmlspecialchars(strip_tags(trim($data)), ENT_QUOTES, 'UTF-8');
+    }    
+    function disconnect() {
+        global $dbLink;
+        mysqli_close($dbLink);
+    }
+    function hashPassword($ps) {
+        $hashedPassword = md5(sha1(md5(base64_encode(md5(md5(sha1(sha1(md5($ps)))))))));
+        return $hashedPassword;
+    }
+?>
