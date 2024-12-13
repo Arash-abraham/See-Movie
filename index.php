b5e5df24800aa834162aed0e8ce1ee23694970c3
Arash <82562395+Arash-abraham@users.noreply.github.com>
2024-12-13 14:57:53 +0330

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+require 'routes.php';
+
+$requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+foreach ($routes as $route) {
+    if ($route['path'] === $requestUri) {
+        $controller = new $route['controller']();
+        $controller->{$route['method']}();
+        exit;
+    }
+}
+
+http_response_code(404);
+echo "404 Not Found";
